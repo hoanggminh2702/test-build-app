888d3d41114bf320ef96b40817f19aea0331051c
hoanggminh2702 <43093294+hoanggminh2702@users.noreply.github.com>
2022-04-26 13:47:54 +0700

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+import Details from "./pages/details";
+import Home from "./pages/home";
 function App() {
-  return <div className="App">This is Homepage</div>;
+  return (
+    <Routes>
+      <Route path="*" element={<Home />} />
+      <Route path="/details" element={<Details />} />
+    </Routes>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,12 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import Home from "./pages/home";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="*" element={<App />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
